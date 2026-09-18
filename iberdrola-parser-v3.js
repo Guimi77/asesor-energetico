7f623e7b1d98c3e4480ad67bfb1456dfa1dc5de8
Guimi77 <tecnico1@electricabt.com>
2026-09-18 12:29:55 +0200

    Forzar parser Iberdrola validado con facturas reales

--- iberdrola-parser-v3.js
+++ iberdrola-parser-v3.js
@@ -4,7 +4,7 @@
   else root.IBTIberdrolaParser=api;
 })(typeof globalThis!=='undefined'?globalThis:this,function(){
   'use strict';
-  const REVISION='2026.09.18.7';
+  const REVISION='2026.09.18.8';
   const round2=n=>Math.round((Number(n)||0)*100)/100;
   const clean=s=>String(s??'').replace(/\u00a0/g,' ').replace(/\s+/g,' ').trim();
   const norm=s=>clean(s).normalize('NFKD').replace(/[\u0300-\u036f]/g,'');
