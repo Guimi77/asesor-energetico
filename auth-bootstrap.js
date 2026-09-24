010261eead56b362e8548c219cdd31003248df8d
Guimi77 <tecnico1@electricabt.com>
2026-09-24 09:48:42 +0200

    Refresh history UI searchable filters

--- auth-bootstrap.js
+++ auth-bootstrap.js
@@ -185,7 +185,7 @@ window.addEventListener('DOMContentLoaded',()=>{
     window.IBTRecommendationResolution?.installPresentation?.();
     if(document.querySelector('script[data-history-ui]')){loadAnalysisUi();return;}
     const script=document.createElement('script');
-    script.src='history-ui.js?v=20260918-periodscroll2';
+    script.src='history-ui.js?v=20260924-searchfilters1';
     script.dataset.historyUi='1';
     script.onload=()=>{
       loadAnalysisUi();
